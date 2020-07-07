d09d21d95ae54738f85f1eecf0ab1cae70c90929
shecho <shechovala@gmail.com>
2020-07-07 00:31:36 -0500

    welcome view working

---
+++ resources/views/layouts/form.blade.php
@@ -0,0 +1,215 @@
+<!-- consultar autorizacion -->
+<div class="modal fade" id="check-autorization" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+    aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header bg-dark">
+                <h5 class="modal-title text-light" id="ModalLabel">Consultar autorizacion</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <form>
+                    @csrf
+
+                    <input type="hidden" value="{{ csrf_token() }}" id="token-autorization" />
+                    <!-- <input type="hidden" value="{{ csrf_token() }}" id="token" /> -->
+                    <div class="form-group">
+                        <input placeholder="Documento" type="text" class="form-control" id="autorization-document">
+                    </div>
+
+                </form>
+            </div>
+            <div class="p-2 form-group" id="response-autorization">
+                Validation response here
+            </div>
+            <div class="p-2 form-group" id="sever-response-autorization">
+                Server Response Here
+            </div>
+
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancelar</button>
+                <button onclick="autorizationHandler()" id="autorization-btn" type="button" class="btn btn-dark">
+                    Confirmar</button>
+
+            </div
+        </div>
+    </div>
+</div>
+<!--  -->
+
+
+<!-- registrar entrada -->
+<div class="modal fade" id="register-entrace" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+    aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header bg-dark">
+                <h5 class="modal-title text-light" id="ModalLabel1">Registrar Entrada</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <form id="visit-form">
+                    @csrf
+                    <input type="hidden" value="{{ csrf_token() }}" id="token-visit" />
+                    <div class="form-group">
+                        <input placeholder="Nombre Completo" type="text" class="form-control" id="entrace-name">
+                    </div>
+                    <div class="form-group">
+                        <div class="form-group">
+                            <input placeholder="Documento" type="number" class="form-control" id="entrace-document">
+                        </div>
+                        <div class="form-group">
+                            <div class="form-group">
+                                <input placeholder="temperatura" type="number" class="form-control"
+                                    id="entrace-temperature">
+                            </div>
+                            <div class="form-group">
+                                <input disabled min="" max="" type="date" class="form-control" id="entrace-day">
+                            </div>
+                            <div class="form-group">
+                                <input disabled placeholder="Hora" type="time" class="form-control" id="entrace-time">
+                            </div>
+                        </div>
+                    </div>
+                </form>
+            </div>
+            <div class="form-group p-2" id="validation-visit">
+                validation visit here
+            </div>
+            <div class="form-group p-2" id="server-response-visit">
+                restonse response visit here
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancelar</button>
+                <button onclick="visitHandler()" id="confirm-visit" type="button" class="btn btn-dark">
+                    Confirmar</button>
+            </div>
+        </div>
+    </div>
+</div>
+<!--  entrada -->
+
+
+<!-- registrar salida -->
+<div class="modal fade" id="register-exit" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+    aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header bg-dark">
+                <h5 class="modal-title text-light" id="ModalLabel2">Registrar Salida</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <form id="exit-form">
+                    @csrf
+                    <input type="hidden" value="{{ csrf_token() }}" id="token-exit" />
+                    <div class="form-group">
+                        <input placeholder="Nombre Completo" type="text" class="form-control" id="exit-name">
+                    </div>
+                    <div class="form-group">
+                        <div class="form-group">
+                            <input placeholder="Documento" type="number" class="form-control" id="exit-document">
+                        </div>
+                        <div class="form-group">
+                            <div class="form-group">
+                                <input placeholder="temperatura" type="number" class="form-control"
+                                    id="exit-temperature">
+                            </div>
+                            <div class="form-group">
+                                <input disabled min="" max="" type="date" class="form-control" id="exit-day">
+                            </div>
+                            <div class="form-group">
+                                <input disabled placeholder="Hora" type="time" class="form-control" id="exit-time">
+                            </div>
+                        </div>
+                    </div>
+                </form>
+            </div>
+            <div class="form-group p-2" id="response-exit">
+                response exit here
+            </div>
+            <div class="form-group p-2" id="server-response-exit">
+                restonse response exit here
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancelar</button>
+                <button onclick="exitHandler()" id="confirm-exit" type="button" class="btn btn-dark"> Confirmar</button>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- salida -->
+
+<!-- Actualizar datos -->
+<div class="modal fade" id="update-info" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+    aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header bg-dark">
+                <h5 class="modal-title text-light" id="ModalLabel3">Actualizar datos pendientes</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <form id="update-form">
+                    @csrf
+                    <input type="hidden" value="{{ csrf_token() }}" id="token-update" />
+                    <div class="form-group">
+                        <input placeholder="Nombre Completo" type="text" class="form-control" id="update-name">
+                    </div>
+                    <div class="form-group">
+                        <!-- <div class="form-group">
+                            <input placeholder="Documento" type="number" class="form-control" id="update-document">
+                        </div> -->
+                        <div class="form-group">
+                            <input placeholder="Telefono" type="number" class="form-control" id="update-phone">
+                        </div>
+                        <div class="form-group">
+                            <input placeholder="Medio de Transporte" type="text" class="form-control"
+                                id="update-transport">
+                        </div>
+                        <div class="form-group">
+                            <input placeholder="Barrio" type="text" class="form-control" id="update-hood">
+                        </div>
+                        <div class="form-group">
+                            <input placeholder="Municipio" type="text" class="form-control" id="update-city">
+                        </div>
+                        <div class="form-group">
+                            <input placeholder="Programa" type="text" class="form-control" id="update-program">
+                        </div>
+                        <div class="form-group">
+                            <select class="form-control" name="time" id="update-center">
+                                <option value="CDMC">CDMC</option>
+                                <option value="CTM">CTM</option>
+                                <option value="CDCDM">CDCDM</option>
+
+                            </select>
+                            <!-- <input placeholder="Centro" type="text" class="form-control" id="update-center"> -->
+                        </div>
+
+                    </div>
+                </form>
+            </div>
+            <div class="form-group p-2" id="response-update">
+                <h6>response update here </h6>
+            </div>
+            <div class="form-group p-2" id="response-server-update">
+                <h6>response server update here </h6>
+
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancelar</button>
+                <button onclick="UpdateDataHandler()" id="confirm-update" type="button" class="btn btn-dark">
+                    Confirmar</button>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- salida -->
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -8,93 +9,252 @@
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+        <!-- font awesome -->
+        <script src="https://kit.fontawesome.com/d4bbca006e.js" crossorigin="anonymous"></script>
 
         <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+        <link href="{{ asset('css/styleWelcome.css') }}" rel="stylesheet">
 
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
     </head>
+
     <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
+        <header id="header">
+            <div class="d-flex flex-column">
 
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
+                <div class="profile">
+                    <img src="{{ asset('img/senaIcon.png') }}" alt="" class="bg-light img-fluid rounded-circle">
 
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
+                    <h1 class="text-light"><a href="/">SENA Antioquia </a></h1>
+                    <div class="social-links mt-3 text-center">
+                        <a href="#" class="twitter">
+                            <i class="fa fa-twitter" aria-hidden="true"></i>
+                        </a>
+                        <a href="#" class="facebook">
+                            <i class="fa fa-facebook-official" aria-hidden="true"></i>
+                        </a>
+                        <a href="#" class="instagram">
+                            <i class="fa fa-instagram" aria-hidden="true"></i>
+                        </a>
+                        <a href="#" class="linkedin">
+                            <i class="fa fa-linkedin" aria-hidden="true"></i>
+                        </a>
+                    </div>
                 </div>
 
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
+                <nav class="nav-menu">
+                    <ul>
+                        <li class="active"><a href="/"><i class="text-light fa fa-home" aria-hidden="true">
+                                </i><span>Inicio</span></a>
+                        </li>
+                        <li><a href="#about">
+                                <i class="text-light fa fa-user" aria-hidden="true"></i>
+                                <span>Nosotros</span></a>
+                        </li>
+                        <li><a href="#resume"><i class="text-light fa fa-database"
+                                    aria-hidden="true"></i><span>Datos</span></a>
+                        </li>
+                        <!-- <li><a href="#portfolio"><i class="bx bx-book-content"></i> Portafolio </a></li>
+                        <li><a href="#services"><i class="bx bx-server"></i> Servicios</a></li>
+                        <li><a href="#contact"><i class="bx bx-envelope"></i> Contacto</a></li> -->
+                        <button id="autorization" typide="button"
+                            class="m-2 p-1 entraceButton btn btn-outline-dark btn-xl text-bold text-uppercase js-scroll-trigger"
+                            data-toggle="modal" data-target="#check-autorization" data-whatever="@mdo"> Consultar
+                            Autorizacion
+                        </button>
+                        <br>
+                        <button onclick="getTimeAndHour()" id="entrace" typide="button"
+                            class="m-2 p-1 entraceButton btn btn-outline-dark btn-xl text-bold text-uppercase js-scroll-trigger"
+                            data-toggle="modal" data-target="#register-entrace" data-whatever="@mdo"> Registrar Entrada
+                        </button>
+                        <br>
+                        <button onclick="getTimeAndHourExit()" id="exit" typide="button"
+                            class="m-2 p-1  entraceButton btn btn-outline-dark  text-bold  js-scroll-trigger"
+                            data-toggle="modal" data-target="#register-exit" data-whatever="@mdo"> Registrar Salida
+                        </button>
+                        <br>
+                        <button id="uptadeData" typide="button"
+                            class="m-2 p-1 entraceButton btn btn-outline-dark btn-xl text-bold text-uppercase js-scroll-trigger"
+                            data-toggle="modal" data-target="#update-info" data-whatever="@mdo"> Actualizar Datos
+                        </button>
+
+                        <div class="d-flex">
+                            @if (Route::has('login'))
+                            <div class="m-2 p-1">
+                                <div class="d-flex">
+                                    <!-- <div class="d-flex container">
+
+                                    </div> -->
+
+                                    @auth
+                                    <a href="{{ url('/home') }}">Home</a>
+                                    @else
+                                    <a class="m-1 btn btn-outline-dark p-2" href="{{ route('login') }}">Login</a>
+
+                                    @if (Route::has('register'))
+                                    <a class="m-1 btn btn-outline-dark p-2 mr-5"
+                                        href="{{ route('register') }}">Register</a>
+                                    @endif
+                                    @endauth
+                                    <button id="displayTogler" type="button" class="mobile-nav-toggle d-xl-none">
+
+                                        <i class="text-dark icofont-navigation-menu fa fa-bars" aria-hidden="true"></i>
+                                    </button>
+
+                                </div>
+                                @endif
+
+                            </div>
+                        </div>
+                    </ul>
+                </nav><!-- .nav-menu -->
+                <button type="button" class="mobile-nav-toggle d-xl-none"><i class="icofont-navigation-menu"></i>
+                </button>
+
             </div>
-        </div>
+            <!-- Login and Register -->
+
+            <!--  -->
+        </header><!-- End Header -->
+        <div id="main">
+            <section id="hero" class="d-flex flex-column justify-content-center align-items-center">
+                <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+                <h1 class="text-dark">Kratos</h1>
+                <h2 class="text-dark">Complejo Sur <span class="typed"
+                        data-typed-items="Usa tapabocas,Lava tus manos, Quedate en casa"></span></h2>
+    
+            </section><!-- End Hero -->
+           
+    
+                <!-- ======= About Section ======= -->
+                <section id="about" class="about">
+                    <!-- <div class="container">
+                    </div> -->
+                </section><!-- End About Section -->
+    
+                <section id="facts" class="facts">
+                    <div class="container">
+            
+                      <div class="section-title">
+                        <h2>Algunos datos</h2>
+                        <p>Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sint
+                          consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea. Quia fugiat
+                          sit in iste officiis commodi quidem hic quas.</p>
+                      </div>
+            
+                      <div class="row no-gutters">
+            
+                        <div class="col-lg-3 col-md-6 d-md-flex align-items-md-stretch" data-aos="fade-up">
+                          <div class="count-box">
+                            <i class="text-dark fa fa-users" aria-hidden="true"></i>
+                            <span data-toggle="counter-up">232</span>
+                            <p><strong>Aprendices</strong> </p>
+                          </div>
+                        </div>
+            
+                        <div class="col-lg-3 col-md-6 d-md-flex align-items-md-stretch" data-aos="fade-up" data-aos-delay="100">
+                          <div class="count-box">
+                            <i class="text-dark fas fa-user-shield"></i>
+                            <span data-toggle="counter-up">521</span>
+                            <p><strong>InstructoresS</strong> </p>
+                          </div>
+                        </div>
+            
+            
+                        <div class="col-lg-3 col-md-6 d-md-flex align-items-md-stretch" data-aos="fade-up" data-aos-delay="300">
+                          <div class="count-box">
+                            <i class="text-dark fas fa-user-tie"></i>
+                            <span data-toggle="counter-up">115</span>
+                            <p><strong>Colaboradores</strong> </p>
+                          </div>
+                        </div>
+    
+                        <div class="col-lg-3 col-md-6 d-md-flex align-items-md-stretch" data-aos="fade-up" data-aos-delay="200">
+                          <div class="count-box">
+                            <i class="text-dark fas fa-temperature-low"></i>
+                            <span data-toggle="counter-up">35.5</span>
+                            <p><strong>Temperatura Promedio</strong> </p>
+                          </div>
+                        </div>
+                      </div>
+            
+                    </div>
+                  </section><!-- End Facts Section -->
+                  <section id="skills" class="skills section-bg">
+                    <div class="container">
+            
+                      <div class="section-title">
+                        <h2>Estadisticas</h2>
+                        <p>Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sint
+                          consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea. Quia fugiat
+                          sit in iste officiis commodi quidem hic quas.</p>
+                      </div>
+            
+                      <div class="row skills-content">
+            
+                        <div class="col-lg-6" data-aos="fade-up">
+            
+                          <div class="progress">
+                            <span class="skill">HTML <i class="val">90%</i></span>
+                            <div class="progress-bar-wrap">
+                              <div class="progress-bar" role="progressbar" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100">
+                              </div>
+                            </div>
+                          </div>
+            
+                          <div class="progress">
+                            <span class="skill">CSS <i class="val">90%</i></span>
+                            <div class="progress-bar-wrap">
+                              <div class="progress-bar" role="progressbar" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100">
+                              </div>
+                            </div>
+                          </div>
+            
+                          <div class="progress">
+                            <span class="skill">JavaScript <i class="val">85%</i></span>
+                            <div class="progress-bar-wrap">
+                              <div class="progress-bar" role="progressbar" aria-valuenow="85" aria-valuemin="0" aria-valuemax="100">
+                              </div>
+                            </div>
+                          </div>
+            
+                        </div>
+            
+                        <div class="col-lg-6" data-aos="fade-up" data-aos-delay="100">
+            
+                          <div class="progress">
+                            <span class="skill">PHP <i class="val">80%</i></span>
+                            <div class="progress-bar-wrap">
+                              <div class="progress-bar" role="progressbar" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100">
+                              </div>
+                            </div>
+                          </div>
+            
+                          <div class="progress">
+                            <span class="skill">Node <i class="val">60%</i></span>
+                            <div class="progress-bar-wrap">
+                              <div class="progress-bar" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100">
+                              </div>
+                            </div>
+                          </div>
+            
+                          <div class="progress">
+                            <span class="skill">Bootstrap <i class="val">80%</i></span>
+                            <div class="progress-bar-wrap">
+                              <div class="progress-bar" role="progressbar" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100">
+                              </div>
+                            </div>
+                          </div>
+            
+                        </div>
+            
+                      </div>
+            
+                    </div>
+                  </section><!-- End Skills Section --> 
+                  <!-- @extends('layouts.form') -->
+            </div>
+            
     </body>
+
 </html>
