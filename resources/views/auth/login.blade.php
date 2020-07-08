311c953887191fa11138714311a203e51f8653f0
shecho <shechovala@gmail.com>
2020-07-08 18:33:23 -0500

    change to spanish

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -35,12 +35,12 @@
 <body class="hold-transition login-page">
 <div class="login-box">
     <div class="login-logo">
-        <a href="{{ url('/home') }}"><b>InfyOm </b>Generator</a>
+        <a href="{{ url('/home') }}"><b></b></a>
     </div>
 
     <!-- /.login-logo -->
     <div class="login-box-body">
-        <p class="login-box-msg">Sign in to start your session</p>
+        <p class="login-box-msg">Inicio sesión</p>
 
         <form method="post" action="{{ url('/login') }}">
             @csrf
